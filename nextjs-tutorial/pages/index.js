d9aaf3b8d0a301df504bd27d112f60484ba15ffb
NANGGI <mqsora@naver.com>
2022-01-13 18:15:29 +0900

    Add files via upload

---
+++ nextjs-tutorial/pages/index.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import ItemList from '../src/component/ItemList';
+import { Divider, Header } from 'semantic-ui-react';
+
+export default function Home() {
+  const [list, setList] = useState();
+  const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+
+  const getData = () => {
+    axios.get(API_URL).then((res) => {
+      console.log(res.data);
+      setList(res.data);
+    })
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  return (
+    <div>
+      <Head>
+        <title>HOME | 코딩앙마</title>
+      </Head>
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        베스트 상품
+      </Header>
+      <Divider />
+      <ItemList list={list} />
+    </div>
+  )
+}
---
+++ nextjs-tutorial/src/component/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div>Copyright &copy; 코딩앙마. All rights reserved.</div>;
+}
