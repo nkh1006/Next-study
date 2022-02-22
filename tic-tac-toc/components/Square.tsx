c574766ef084ff1ec64d8ef0e393ac858df7d25e
NANGGI <mqsora@naver.com>
2022-02-22 18:27:41 +0900

    Add files via upload

---
+++ tic-tac-toc/components/Square.tsx
@@ -0,0 +1,16 @@
+type Player = 'X' | 'O' | null;
+
+const Square = ({
+  value, onClick, winner
+} : {
+  winner: string | null;
+  value: string | null;
+  onClick: () => void;
+}) => {
+  if(!value){
+    return <button className="square" onClick={onClick} disabled={Boolean(winner)} />
+  }
+  return <button className={`square square_${value.toLowerCase()}`} disabled>{value}</button>
+};
+
+export default Square;
