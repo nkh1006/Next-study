b44759d66188e8e0ff863862e55ad9ec9663a16c
NANGGI <mqsora@naver.com>
2022-03-08 22:47:41 +0900

    Add files via upload

---
+++ next-starwas/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl text-center mt-6 font-semibold my-6">Welcome to Next.js Stars API</h1>
+      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat leo sit amet quam dictum vulputate. Duis quis erat molestie ligula vehicula molestie ut vestibulum sapien. Donec sollicitudin neque vitae erat rutrum, vitae condimentum augue imperdiet. Proin dignissim velit a nisi lacinia, a consectetur lacus molestie. Fusce luctus auctor eros sed molestie. Cras purus leo, aliquam eget eros sed, malesuada facilisis mauris. In lacinia congue turpis quis dignissim. Donec lobortis congue felis eget consequat.</p>
+
+      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat leo sit amet quam dictum vulputate. Duis quis erat molestie ligula vehicula molestie ut vestibulum sapien. Donec sollicitudin neque vitae erat rutrum, vitae condimentum augue imperdiet. Proin dignissim velit a nisi lacinia, a consectetur lacus molestie. Fusce luctus auctor eros sed molestie. Cras purus leo, aliquam eget eros sed, malesuada facilisis mauris. In lacinia congue turpis quis dignissim. Donec lobortis congue felis eget consequat.</p>
+
+      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat leo sit amet quam dictum vulputate. Duis quis erat molestie ligula vehicula molestie ut vestibulum sapien. Donec sollicitudin neque vitae erat rutrum, vitae condimentum augue imperdiet. Proin dignissim velit a nisi lacinia, a consectetur lacus molestie. Fusce luctus auctor eros sed molestie. Cras purus leo, aliquam eget eros sed, malesuada facilisis mauris. In lacinia congue turpis quis dignissim. Donec lobortis congue felis eget consequat.</p>
+    </div>
+  )
+}
