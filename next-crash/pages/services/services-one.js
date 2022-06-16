62d42e986ad1960866721e8bdcb35bcc515a9173
NANGGI <mqsora@naver.com>
2022-06-17 00:59:06 +0900

    Add files via upload

---
+++ next-crash/pages/services/services-one.js
@@ -0,0 +1,9 @@
+const One = () => {
+  return (
+    <div>
+      <h1>Service One</h1>
+    </div>
+  );
+}
+
+export default One;
---
+++ next-crash/pages/services/services-two.js
@@ -0,0 +1,9 @@
+const Two = () => {
+  return (
+    <div>
+      <h1>Service Two</h1>
+    </div>
+  );
+}
+
+export default Two;
